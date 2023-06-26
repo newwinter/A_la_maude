af9e5a0e47ac1c4f31ea32694fc48b85eff263d7
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-06-26 17:29:13 +0200

    creations tables et ajout insert de la table image

--- backend/database.sql
+++ backend/database.sql
@@ -1,10 +1,8 @@
-
 CREATE TABLE images(
    id  INT  NOT NULL PRIMARY KEY AUTO_INCREMENT
   ,src VARCHAR(200) NOT NULL
   ,alt VARCHAR(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE products(
    id       INT  NOT NULL PRIMARY KEY AUTO_INCREMENT
@@ -27,4 +25,50 @@ CREATE TABLE products_to_categories (
   CONSTRAINT categories_id_fk FOREIGN KEY (categories_id) REFERENCES categories(id),
   CONSTRAINT products_id_fk FOREIGN KEY (products_id) REFERENCES products(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_F_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_F_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_F_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_F_4.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_F_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_F_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_F_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_F_4.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_F_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_F_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_F_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_F_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_M_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_M_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_M_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot1_M_4.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_M_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_M_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_M_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot2_M_4.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_M_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_M_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_M_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/Calot3_M_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_4.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_5.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_6.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton1_7.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton2_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton2_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton2_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton3_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton3_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton3_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton3_4.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton4_1.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton4_2.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton4_3.jpeg','lorem ipsum');
+INSERT INTO images(src,alt) VALUES ('./public/assets/images/coton4_4.jpeg','lorem ipsum');
+
+
 
