17cf1826bc6bd4b8bf7107e3c6925bbf29ef1386
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-07-06 09:35:57 +0200

    modif controllers changement rows par product

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,18 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+router.get("items", itemControllers.browse);
+router.get("items/:id", itemControllers.read);
+router.put("items/:id", itemControllers.edit);
+router.post("items", itemControllers.add);
+router.delete("items/:id", itemControllers.destroy);
+
+const productControllers = require("./controllers/productControllers");
+
+router.get("/products", productControllers.browse);
+router.get("/products/:id", productControllers.read);
+router.put("/products/:id", productControllers.edit);
+router.post("/products", productControllers.add);
+router.delete("/products/:id", productControllers.destroy);
 
 module.exports = router;
