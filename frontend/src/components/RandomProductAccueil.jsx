66b9af40540c039ca6996b8b8b15fdbe5a79993f
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-07-10 16:09:21 +0200

    ajout caroussel page accueil

--- frontend/src/components/RandomProductAccueil.jsx
+++ frontend/src/components/RandomProductAccueil.jsx
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from "react";
+import "./RandomProductAccueil.css";
+// eslint-disable-next-line import/no-unresolved
+import { register } from "swiper/element/bundle";
+
+register();
 
 function RandomProductAccueil() {
   const [products, setProducts] = useState([]);
@@ -13,15 +18,26 @@ function RandomProductAccueil() {
   return (
     <>
       <div>RandomProductAccueil</div>
-      <div>
+      <swiper-container>
         {products.map((product) => (
-          <h1>{product.name}</h1>
-
-          // <ProductCard product={product} />
+          <swiper-slide>
+            <div>
+              {product.name}
+              {product.price}
+            </div>
+            <img
+              src={`${import.meta.env.VITE_BACKEND_URL}/assets/images/${
+                product.src
+              }`}
+              alt={product.alt}
+            />
+          </swiper-slide>
         ))}
-      </div>
+      </swiper-container>
     </>
   );
 }
 
 export default RandomProductAccueil;
+
+// http://localhost:5005/assets/images/Calot2_F_1.jpeg
